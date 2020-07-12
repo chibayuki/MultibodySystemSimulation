84fa37c6858e3146b5e11c062a9b57b0c4707cf9
千夏茹初 <chibayuki@foxmail.com>
2020-07-12 16:02:13 +0800

    Add files via upload

--- Multibody/MainForm.Designer.cs
+++ Multibody/MainForm.Designer.cs
@@ -29,28 +29,136 @@
         private void InitializeComponent()
         {
             this.Panel_Main = new System.Windows.Forms.Panel();
+            this.Panel_SideBar = new System.Windows.Forms.Panel();
+            this.Label_RotateZ = new System.Windows.Forms.Label();
+            this.Label_RotateY = new System.Windows.Forms.Label();
+            this.Label_RotateX = new System.Windows.Forms.Label();
+            this.Label_OffsetZ = new System.Windows.Forms.Label();
+            this.Label_OffsetY = new System.Windows.Forms.Label();
+            this.Label_OffsetX = new System.Windows.Forms.Label();
+            this.Panel_View = new System.Windows.Forms.Panel();
+            this.Panel_Main.SuspendLayout();
+            this.Panel_SideBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // Panel_Main
             // 
             this.Panel_Main.BackColor = System.Drawing.Color.Transparent;
+            this.Panel_Main.Controls.Add(this.Panel_SideBar);
+            this.Panel_Main.Controls.Add(this.Panel_View);
             this.Panel_Main.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Panel_Main.Location = new System.Drawing.Point(0, 0);
             this.Panel_Main.Name = "Panel_Main";
-            this.Panel_Main.Size = new System.Drawing.Size(300, 300);
+            this.Panel_Main.Size = new System.Drawing.Size(1200, 800);
             this.Panel_Main.TabIndex = 0;
-            this.Panel_Main.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_Main_Paint);
+            // 
+            // Panel_SideBar
+            // 
+            this.Panel_SideBar.BackColor = System.Drawing.Color.Transparent;
+            this.Panel_SideBar.Controls.Add(this.Label_RotateZ);
+            this.Panel_SideBar.Controls.Add(this.Label_RotateY);
+            this.Panel_SideBar.Controls.Add(this.Label_RotateX);
+            this.Panel_SideBar.Controls.Add(this.Label_OffsetZ);
+            this.Panel_SideBar.Controls.Add(this.Label_OffsetY);
+            this.Panel_SideBar.Controls.Add(this.Label_OffsetX);
+            this.Panel_SideBar.Location = new System.Drawing.Point(900, 0);
+            this.Panel_SideBar.Name = "Panel_SideBar";
+            this.Panel_SideBar.Size = new System.Drawing.Size(300, 800);
+            this.Panel_SideBar.TabIndex = 0;
+            // 
+            // Label_RotateZ
+            // 
+            this.Label_RotateZ.BackColor = System.Drawing.Color.Transparent;
+            this.Label_RotateZ.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_RotateZ.ForeColor = System.Drawing.Color.White;
+            this.Label_RotateZ.Location = new System.Drawing.Point(200, 80);
+            this.Label_RotateZ.Name = "Label_RotateZ";
+            this.Label_RotateZ.Size = new System.Drawing.Size(80, 30);
+            this.Label_RotateZ.TabIndex = 0;
+            this.Label_RotateZ.Text = "Rz";
+            this.Label_RotateZ.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label_RotateY
+            // 
+            this.Label_RotateY.BackColor = System.Drawing.Color.Transparent;
+            this.Label_RotateY.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_RotateY.ForeColor = System.Drawing.Color.White;
+            this.Label_RotateY.Location = new System.Drawing.Point(110, 80);
+            this.Label_RotateY.Name = "Label_RotateY";
+            this.Label_RotateY.Size = new System.Drawing.Size(80, 30);
+            this.Label_RotateY.TabIndex = 0;
+            this.Label_RotateY.Text = "Ry";
+            this.Label_RotateY.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label_RotateX
+            // 
+            this.Label_RotateX.BackColor = System.Drawing.Color.Transparent;
+            this.Label_RotateX.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_RotateX.ForeColor = System.Drawing.Color.White;
+            this.Label_RotateX.Location = new System.Drawing.Point(20, 80);
+            this.Label_RotateX.Name = "Label_RotateX";
+            this.Label_RotateX.Size = new System.Drawing.Size(80, 30);
+            this.Label_RotateX.TabIndex = 0;
+            this.Label_RotateX.Text = "Rx";
+            this.Label_RotateX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label_OffsetZ
+            // 
+            this.Label_OffsetZ.BackColor = System.Drawing.Color.Transparent;
+            this.Label_OffsetZ.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_OffsetZ.ForeColor = System.Drawing.Color.White;
+            this.Label_OffsetZ.Location = new System.Drawing.Point(200, 40);
+            this.Label_OffsetZ.Name = "Label_OffsetZ";
+            this.Label_OffsetZ.Size = new System.Drawing.Size(80, 30);
+            this.Label_OffsetZ.TabIndex = 0;
+            this.Label_OffsetZ.Text = "Δz";
+            this.Label_OffsetZ.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label_OffsetY
+            // 
+            this.Label_OffsetY.BackColor = System.Drawing.Color.Transparent;
+            this.Label_OffsetY.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_OffsetY.ForeColor = System.Drawing.Color.White;
+            this.Label_OffsetY.Location = new System.Drawing.Point(110, 40);
+            this.Label_OffsetY.Name = "Label_OffsetY";
+            this.Label_OffsetY.Size = new System.Drawing.Size(80, 30);
+            this.Label_OffsetY.TabIndex = 0;
+            this.Label_OffsetY.Text = "Δy";
+            this.Label_OffsetY.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label_OffsetX
+            // 
+            this.Label_OffsetX.BackColor = System.Drawing.Color.Transparent;
+            this.Label_OffsetX.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_OffsetX.ForeColor = System.Drawing.Color.White;
+            this.Label_OffsetX.Location = new System.Drawing.Point(20, 40);
+            this.Label_OffsetX.Name = "Label_OffsetX";
+            this.Label_OffsetX.Size = new System.Drawing.Size(80, 30);
+            this.Label_OffsetX.TabIndex = 0;
+            this.Label_OffsetX.Text = "Δx";
+            this.Label_OffsetX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Panel_View
+            // 
+            this.Panel_View.BackColor = System.Drawing.Color.Transparent;
+            this.Panel_View.Location = new System.Drawing.Point(0, 0);
+            this.Panel_View.Name = "Panel_View";
+            this.Panel_View.Size = new System.Drawing.Size(1200, 800);
+            this.Panel_View.TabIndex = 0;
+            this.Panel_View.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_View_Paint);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.ClientSize = new System.Drawing.Size(1200, 800);
             this.Controls.Add(this.Panel_Main);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Panel_Main.ResumeLayout(false);
+            this.Panel_SideBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -58,5 +166,13 @@
         #endregion
 
         private System.Windows.Forms.Panel Panel_Main;
+        private System.Windows.Forms.Panel Panel_View;
+        private System.Windows.Forms.Panel Panel_SideBar;
+        private System.Windows.Forms.Label Label_OffsetX;
+        private System.Windows.Forms.Label Label_RotateZ;
+        private System.Windows.Forms.Label Label_RotateY;
+        private System.Windows.Forms.Label Label_RotateX;
+        private System.Windows.Forms.Label Label_OffsetZ;
+        private System.Windows.Forms.Label Label_OffsetY;
     }
 }
--- Multibody/MainForm.cs
+++ Multibody/MainForm.cs
@@ -30,7 +30,6 @@ using Geometry = Com.Geometry;
 using Painting2D = Com.Painting2D;
 using PointD = Com.PointD;
 using PointD3D = Com.PointD3D;
-using PointD6D = Com.PointD6D;
 using Statistics = Com.Statistics;
 using Texting = Com.Text;
 using VectorType = Com.Vector.Type;
@@ -90,11 +89,14 @@ namespace Multibody
             Me.EnableCaptionBarTransparent = false;
             Me.Theme = Theme.Black;
             Me.ThemeColor = ColorManipulation.GetRandomColorX();
-            Me.Size = new Size(1500, 1000);
 
             Me.Loading += Me_Loading;
             Me.Loaded += Me_Loaded;
             Me.Closed += Me_Closed;
+            Me.Resize += Me_Resize;
+            Me.SizeChanged += Me_SizeChanged;
+            Me.ThemeChanged += Me_ThemeChanged;
+            Me.ThemeColorChanged += Me_ThemeChanged;
         }
 
         #endregion
@@ -107,7 +109,7 @@ namespace Multibody
             const int s = 100;
             const int v = 70;
             const int d = 37;
-            int i = 0;
+            /*int i = 0;
 
             _Particles.Add(new Particle(1E7, 7.815926418, new PointD3D(700, 500, 4000), new PointD3D(0, 0.0012, 0), ColorX.FromHSL((h + d * (i++)) % 360, s, v).ToColor()));
             _Particles.Add(new Particle(5E6, 6.203504909, new PointD3D(780, 500, 4000), new PointD3D(0, -0.0021, 0), ColorX.FromHSL((h + d * (i++)) % 360, s, v).ToColor()));
@@ -115,17 +117,85 @@ namespace Multibody
             _Particles.Add(new Particle(5E4, 1.336504618, new PointD3D(420, 500, 4000), new PointD3D(0, -0.0029, 0), ColorX.FromHSL((h + d * (i++)) % 360, s, v).ToColor()));
             _Particles.Add(new Particle(2E5, 2.121568836, new PointD3D(1150, 500, 4000), new PointD3D(0, 0.0017, 0), ColorX.FromHSL((h + d * (i++)) % 360, s, v).ToColor()));
             _Particles.Add(new Particle(1E4, 0.781592642, new PointD3D(1170, 500, 4000), new PointD3D(0, 0.0024, 0), ColorX.FromHSL((h + d * (i++)) % 360, s, v).ToColor()));
-            _Particles.Add(new Particle(2E4, 0.984745022, new PointD3D(320, 500, 4000), new PointD3D(0, 0.0017, 0), ColorX.FromHSL((h + d * (i++)) % 360, s, v).ToColor()));
+            _Particles.Add(new Particle(2E4, 0.984745022, new PointD3D(320, 500, 4000), new PointD3D(0, 0.0017, 0), ColorX.FromHSL((h + d * (i++)) % 360, s, v).ToColor()));*/
+
+            for (int i = 0; i < 10; i++)
+            {
+                _Particles.Add(new Particle(Statistics.RandomDouble(2E5, 5E6), Statistics.RandomDouble(2, 5), new PointD3D(Statistics.RandomDouble(200, 1000), Statistics.RandomDouble(200, 600), Statistics.RandomDouble(-1000, 1000)), new PointD3D(Statistics.RandomDouble(0.0005, 0.001), Statistics.RandomDouble(2 * Math.PI), Statistics.RandomDouble(2 * Math.PI)).ToCartesian(), ColorX.FromHSL((h + d * (i++)) % 360, s, v).ToColor()));
+            }
         }
 
         private void Me_Loaded(object sender, EventArgs e)
         {
+            Me.OnThemeChanged();
+            Me.OnSizeChanged();
+
+            Label_OffsetX.MouseEnter += Label_ViewOperation_MouseEnter;
+            Label_OffsetY.MouseEnter += Label_ViewOperation_MouseEnter;
+            Label_OffsetZ.MouseEnter += Label_ViewOperation_MouseEnter;
+            Label_RotateX.MouseEnter += Label_ViewOperation_MouseEnter;
+            Label_RotateY.MouseEnter += Label_ViewOperation_MouseEnter;
+            Label_RotateZ.MouseEnter += Label_ViewOperation_MouseEnter;
+
+            Label_OffsetX.MouseLeave += Label_ViewOperation_MouseLeave;
+            Label_OffsetY.MouseLeave += Label_ViewOperation_MouseLeave;
+            Label_OffsetZ.MouseLeave += Label_ViewOperation_MouseLeave;
+            Label_RotateX.MouseLeave += Label_ViewOperation_MouseLeave;
+            Label_RotateY.MouseLeave += Label_ViewOperation_MouseLeave;
+            Label_RotateZ.MouseLeave += Label_ViewOperation_MouseLeave;
+
+            Label_OffsetX.MouseDown += Label_ViewOperation_MouseDown;
+            Label_OffsetY.MouseDown += Label_ViewOperation_MouseDown;
+            Label_OffsetZ.MouseDown += Label_ViewOperation_MouseDown;
+            Label_RotateX.MouseDown += Label_ViewOperation_MouseDown;
+            Label_RotateY.MouseDown += Label_ViewOperation_MouseDown;
+            Label_RotateZ.MouseDown += Label_ViewOperation_MouseDown;
+
+            Label_OffsetX.MouseUp += Label_ViewOperation_MouseUp;
+            Label_OffsetY.MouseUp += Label_ViewOperation_MouseUp;
+            Label_OffsetZ.MouseUp += Label_ViewOperation_MouseUp;
+            Label_RotateX.MouseUp += Label_ViewOperation_MouseUp;
+            Label_RotateY.MouseUp += Label_ViewOperation_MouseUp;
+            Label_RotateZ.MouseUp += Label_ViewOperation_MouseUp;
+
+            Label_OffsetX.MouseMove += Label_OffsetX_MouseMove;
+            Label_OffsetY.MouseMove += Label_OffsetY_MouseMove;
+            Label_OffsetZ.MouseMove += Label_OffsetZ_MouseMove;
+            Label_RotateX.MouseMove += Label_RotateX_MouseMove;
+            Label_RotateY.MouseMove += Label_RotateY_MouseMove;
+            Label_RotateZ.MouseMove += Label_RotateZ_MouseMove;
+
             RedrawThreadStart();
         }
 
         private void Me_Closed(object sender, EventArgs e)
         {
             RedrawThreadStop();
+        }
+
+        private void Me_Resize(object sender, EventArgs e)
+        {
+            Panel_SideBar.Height = Panel_Main.Height;
+            Panel_SideBar.Left = Panel_Main.Width - Panel_SideBar.Width;
+        }
+
+        private void Me_SizeChanged(object sender, EventArgs e)
+        {
+            Me.OnResize();
+
+            Panel_View.Size = Panel_Main.Size;
+        }
+
+        private void Me_ThemeChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Me.RecommendColors.FormBackground.ToColor();
+
+            Panel_View.BackColor = Me.RecommendColors.FormBackground.ToColor();
+
+            Panel_SideBar.BackColor = Me.RecommendColors.Background_DEC.ToColor();
+
+            Label_OffsetX.ForeColor = Label_OffsetY.ForeColor = Label_OffsetZ.ForeColor = Label_RotateX.ForeColor = Label_RotateY.ForeColor = Label_RotateZ.ForeColor = Me.RecommendColors.Text_INC.ToColor();
+            Label_OffsetX.BackColor = Label_OffsetY.BackColor = Label_OffsetZ.BackColor = Label_RotateX.BackColor = Label_RotateY.BackColor = Label_RotateZ.BackColor = Me.RecommendColors.Background_INC.ToColor();
         }
 
         #endregion
@@ -149,10 +219,13 @@ namespace Multibody
         private AffineTransformation _AffineTransformationCopy = null; // 视图控制开始前使用的仿射变换的副本。
 
         // 仿射变换使用的坐标系原点（屏幕坐标系）。
-        private PointD3D AffineTransformCenter => new PointD3D(Panel_Main.Width / 2, Panel_Main.Height / 2, 0);
+        private PointD3D AffineTransformCenter => new PointD3D(Panel_View.Width / 2, Panel_View.Height / 2, 0);
+
+        // 仿射变换使用的坐标系原点（屏幕坐标系）。
+        private PointD3D ProjectCenter => new PointD3D(Panel_View.Width / 2, Panel_View.Height / 2, 0);
 
         // 投影变换使用的焦距。
-        private double FocalLength => new PointD(Panel_Main.Size).Module;
+        private double FocalLength => new PointD(Panel_View.Size).Module;
 
         // 视图控制开始。
         private void ViewOperationStart()
@@ -163,63 +236,167 @@ namespace Multibody
         // 视图控制停止。
         private void ViewOperationStop()
         {
-            _AffineTransformation = AffineTransformation.FromMatrixTransform(_AffineTransformationCopy.ToMatrix(VectorType.ColumnVector, 3));
             _AffineTransformationCopy = null;
         }
 
-        // 视图控制更新六自由度（Δx，Δy，Δz，Rx，Ry，Rz）参数。
-        private void ViewOperationUpdateParam(PointD6D param)
+        // 视图控制类型。
+        private enum ViewOperationType
         {
-            AffineTransformation affineTransformation = _AffineTransformationCopy.Copy();
+            OffsetX,
+            OffsetY,
+            OffsetZ,
+            RotateX,
+            RotateY,
+            RotateZ
+        }
 
-            if (param.X != 0)
+        // 视图控制更新参数。
+        private void ViewOperationUpdateParam(ViewOperationType type, double value)
+        {
+            if (value != 0)
             {
-                affineTransformation.Offset(0, param.X);
-            }
+                AffineTransformation affineTransformation = _AffineTransformationCopy.Copy();
 
-            if (param.Y != 0)
-            {
-                affineTransformation.Offset(1, param.Y);
-            }
-
-            if (param.Z != 0)
-            {
-                affineTransformation.Offset(2, param.Z);
-            }
-
-            if (param.U != 0 || param.V != 0 || param.W != 0)
-            {
-                PointD3D center = AffineTransformCenter;
-
-                affineTransformation.Offset(0, -center.X);
-                affineTransformation.Offset(1, -center.Y);
-                affineTransformation.Offset(2, -center.Z);
-
-                if (param.U != 0)
+                if (type <= ViewOperationType.OffsetZ)
                 {
-                    affineTransformation.Rotate(1, 2, param.U);
-                }
-                else if (param.V != 0)
-                {
-                    affineTransformation.Rotate(2, 0, param.V);
+                    switch (type)
+                    {
+                        case ViewOperationType.OffsetX: affineTransformation.Offset(0, value); break;
+                        case ViewOperationType.OffsetY: affineTransformation.Offset(1, value); break;
+                        case ViewOperationType.OffsetZ: affineTransformation.Offset(2, value); break;
+                    }
                 }
                 else
                 {
-                    affineTransformation.Rotate(0, 1, param.W);
+                    PointD3D center = AffineTransformCenter;
+
+                    affineTransformation.Offset(0, -center.X);
+                    affineTransformation.Offset(1, -center.Y);
+                    affineTransformation.Offset(2, -center.Z);
+
+                    switch (type)
+                    {
+                        case ViewOperationType.RotateX: affineTransformation.Rotate(1, 2, value); break;
+                        case ViewOperationType.RotateY: affineTransformation.Rotate(2, 0, value); break;
+                        case ViewOperationType.RotateZ: affineTransformation.Rotate(0, 1, value); break;
+                    }
+
+                    affineTransformation.Offset(0, center.X);
+                    affineTransformation.Offset(1, center.Y);
+                    affineTransformation.Offset(2, center.Z);
                 }
 
-                affineTransformation.Offset(0, center.X);
-                affineTransformation.Offset(1, center.Y);
-                affineTransformation.Offset(2, center.Z);
+                _AffineTransformation = AffineTransformation.FromMatrixTransform(affineTransformation.ToMatrix(VectorType.ColumnVector, 3));
             }
-
-            _AffineTransformation = AffineTransformation.FromMatrixTransform(affineTransformation.ToMatrix(VectorType.ColumnVector, 3));
         }
 
         // 世界坐标系转换到屏幕坐标系。
         private PointD WorldToScreen(PointD3D pt)
         {
-            return pt.AffineTransformCopy(_AffineTransformation).ProjectToXY(AffineTransformCenter, FocalLength);
+            return pt.AffineTransformCopy(_AffineTransformation).ProjectToXY(ProjectCenter, FocalLength);
+        }
+
+        private const double _ShiftPerPixel = 1; // 每像素的偏移量。
+        private const double _RadPerPixel = Math.PI / 180; // 每像素的旋转弧度。
+
+        private int _CursorX = 0; // 鼠标指针 X 坐标。
+        private bool _AdjustNow = false; // 是否正在调整。
+
+        private void Label_ViewOperation_MouseEnter(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = Me.RecommendColors.Button_DEC.ToColor();
+        }
+
+        private void Label_ViewOperation_MouseLeave(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = Me.RecommendColors.Background_INC.ToColor();
+        }
+
+        private void Label_ViewOperation_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ((Label)sender).BackColor = Me.RecommendColors.Button_INC.ToColor();
+                ((Label)sender).Cursor = Cursors.SizeWE;
+
+                ViewOperationStart();
+
+                _CursorX = e.X;
+                _AdjustNow = true;
+            }
+        }
+
+        private void Label_ViewOperation_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _AdjustNow = false;
+
+                ViewOperationStop();
+
+                ((Label)sender).BackColor = (Geometry.CursorIsInControl((Label)sender) ? Me.RecommendColors.Button_DEC.ToColor() : Me.RecommendColors.Background_INC.ToColor());
+                ((Label)sender).Cursor = Cursors.Default;
+            }
+        }
+
+        private void Label_OffsetX_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_AdjustNow)
+            {
+                double off = (e.X - _CursorX) * _ShiftPerPixel;
+
+                ViewOperationUpdateParam(ViewOperationType.OffsetX, off);
+            }
+        }
+
+        private void Label_OffsetY_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_AdjustNow)
+            {
+                double off = (e.X - _CursorX) * _ShiftPerPixel;
+
+                ViewOperationUpdateParam(ViewOperationType.OffsetY, off);
+            }
+        }
+
+        private void Label_OffsetZ_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_AdjustNow)
+            {
+                double off = (e.X - _CursorX) * _ShiftPerPixel;
+
+                ViewOperationUpdateParam(ViewOperationType.OffsetZ, off);
+            }
+        }
+
+        private void Label_RotateX_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_AdjustNow)
+            {
+                double rot = (e.X - _CursorX) * _RadPerPixel;
+
+                ViewOperationUpdateParam(ViewOperationType.RotateX, rot);
+            }
+        }
+
+        private void Label_RotateY_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_AdjustNow)
+            {
+                double rot = (e.X - _CursorX) * _RadPerPixel;
+
+                ViewOperationUpdateParam(ViewOperationType.RotateY, rot);
+            }
+        }
+
+        private void Label_RotateZ_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_AdjustNow)
+            {
+                double rot = (e.X - _CursorX) * _RadPerPixel;
+
+                ViewOperationUpdateParam(ViewOperationType.RotateZ, rot);
+            }
         }
 
         #endregion
@@ -236,14 +413,14 @@ namespace Multibody
                 _MultibodyBitmap.Dispose();
             }
 
-            _MultibodyBitmap = new Bitmap(Math.Max(1, Panel_Main.Width), Math.Max(1, Panel_Main.Height));
+            _MultibodyBitmap = new Bitmap(Math.Max(1, Panel_View.Width), Math.Max(1, Panel_View.Height + Me.CaptionBarHeight));
 
             if (_MultibodySystem != null)
             {
                 using (Graphics Grap = Graphics.FromImage(_MultibodyBitmap))
                 {
                     Grap.SmoothingMode = SmoothingMode.AntiAlias;
-                    Grap.Clear(Color.Black);
+                    Grap.Clear(Panel_View.BackColor);
 
                     RectangleF bitmapBounds = new RectangleF(new PointF(), _MultibodyBitmap.Size);
 
@@ -306,11 +483,11 @@ namespace Multibody
             {
                 Me.CaptionBarBackgroundImage = _MultibodyBitmap;
 
-                Panel_Main.CreateGraphics().DrawImage(_MultibodyBitmap, new Point(0, -Me.CaptionBarHeight));
+                Panel_View.CreateGraphics().DrawImage(_MultibodyBitmap, new Point(0, -Me.CaptionBarHeight));
             }
         }
 
-        private void Panel_Main_Paint(object sender, PaintEventArgs e)
+        private void Panel_View_Paint(object sender, PaintEventArgs e)
         {
             if (_MultibodyBitmap == null)
             {
